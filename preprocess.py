2686530b93a30d398ac1f5dcb844369915999d8d
yuyan <yuyan_zhao@outlook.com>
2025-05-25 15:03:13 +0200

    Add EDA

--- preprocess.py
+++ preprocess.py
@@ -4,17 +4,10 @@ import numpy as np
 import pandas as pd
 
 
-def filter_df(df_raw: pd.DataFrame) -> pd.DataFrame:
-    # Expand attrs
-    df_attrs = pd.json_normalize(df_raw["attrs"])
-    df = pd.concat([df_raw.drop(columns=["attrs"]), df_attrs], axis=1)
-
-    # Drop unwanted columns
-    df.drop(columns=["car_id", "url", "crawled_at", "Crit'Air"], inplace=True)
-
+def drop_nan_df(df_raw: pd.DataFrame) -> pd.DataFrame:
     # Drop columns that with missing value > 10%
-    cols_to_drop = df.columns[df.isna().mean() > 0.1]
-    df.drop(columns=cols_to_drop, inplace=True)
+    cols_to_drop = df_raw.columns[df_raw.isna().mean() > 0.1]
+    df = df_raw.drop(columns=cols_to_drop)
 
     # Drop rows with NaN values
     df = df.dropna()
@@ -49,7 +42,9 @@ def format_df(df_raw: pd.DataFrame) -> pd.DataFrame:
         df["post_code"].str[:2]
     )
 
-    df["Couleur"] = df["Couleur"].str.lower()
+    cols = df.select_dtypes(include=['object']).columns.tolist()
+    for col in cols:
+        df[col] = df[col].str.lower()
 
     return df
 
@@ -69,5 +64,35 @@ def analyze_df(df: pd.DataFrame):
 
     print("\n-- description --\n")
     print(df.describe())
+
+
+def rare_encode(df: pd.DataFrame, col: str, top_k=5, rare_label="Other"):
+    top_categories = df[col].value_counts().nlargest(top_k).index
+    df[col] = df[col].apply(lambda x: x if x in top_categories else rare_label)
+    df = df.drop(columns=[col])
+    return df
+
+
+def transform_df(df_raw: pd.DataFrame):
+    # Drop unwanted cols
+    unwanted_cols = ["circulation_year", "Puissance fiscale", "brand", "Provenance"]
+    df = df_raw.drop(columns=unwanted_cols)
+
+    # Log transform price
+    df["price"] = np.log1p(df["price"])
+
+    # Binary cols
+    df["gearbox"] = df["gearbox"].map({"Automatique": 1, "Manuelle": 0})
+    df["Première main"] = df["Première main"].map({"Oui": 1, "Non": 0})
+    df["Contrôle technique"] = df["Contrôle technique"].map({"Requis": 1, "Non requis": 0})
+
+    df["Norme euro"] = df["Norme euro"].str[-1].astype(int)
+    df["Couleur"] = df["Couleur"].str.split().str[0]
+
+    # Rare Encoding
+    for col in ["energy", "Couleur"]:
+        rare_encode(df, col)
+
+    return df
 
 
